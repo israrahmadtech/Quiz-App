8bf13cd2894b0e9c159a3d03741a0afc7120b053
israrahmadtech <israrahmadtech@gmail.com>
2026-03-07 01:27:09 +0500

    dep err

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: backendUrl,
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": env.VITE_BACKEND_URL
+      }
+    }
+  });
+};
