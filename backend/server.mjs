39b3e2cae6cf8c1b2e737a166e13b973b9eb218c
israrahmadtech <israrahmadtech@gmail.com>
2026-03-07 01:33:58 +0500

    prod fix

--- backend/server.mjs
+++ backend/server.mjs
@@ -18,7 +18,8 @@ const app = express();
 // connect database
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173", credentials: true }));
+app.use(cors("*"));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
